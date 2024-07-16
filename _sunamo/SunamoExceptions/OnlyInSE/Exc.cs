7d474baf522f8e76eea1c42a1150da110c25bc29
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 09:25:24 +0200

    Before removing all *SunamoExceptions.cs

--- _sunamo/SunamoExceptions/OnlyInSE/Exc.cs
+++ _sunamo/SunamoExceptions/OnlyInSE/Exc.cs
@@ -1,9 +1,5 @@
 namespace SunamoAttributes._sunamo.SunamoExceptions.OnlyInSE;
 
-
-
-
-
 internal class Exc
 {
     
--- _sunamo/SunamoValues/AllChars.cs
+++ _sunamo/SunamoValues/AllChars.cs
@@ -1,4 +1,5 @@
 namespace SunamoAttributes._sunamo.SunamoValues;
+
 internal class AllChars
 {
     internal static List<char> vsZnakyWithoutSpecial;
